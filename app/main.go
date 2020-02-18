eaa7fa595ae87aa0ec828ae298a8e93631757bd4
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-02-18 16:06:08 +0530

    Add commented out section to pass first stage

--- app/main.go
+++ app/main.go
@@ -6,4 +6,21 @@ import (
 
 func main() {
 	fmt.Println("Implement your program here")
+
+	// Comment this section out to pass the first stage!
+	//
+	// command := os.Args[3]
+	// args := os.Args[4:len(os.Args)]
+	//
+	// cmd := exec.Command(command, args...)
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+	// err := cmd.Run()
+	//
+	// if err != nil {
+	// 	fmt.Printf("Error in waiting on process: %v\n", err)
+	// 	os.Exit(1)
+	// }
+	//
+	// os.Exit(cmd.ProcessState.ExitCode())
 }
